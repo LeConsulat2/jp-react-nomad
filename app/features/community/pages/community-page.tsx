f0d1371e2cfd9c74c79adca038c11f947237485c
LeConsulat2 <wooine2@gmail.com>
2025-05-08 14:54:10 +1200

    Suspense and Await

--- app/features/community/pages/community-page.tsx
+++ app/features/community/pages/community-page.tsx
@@ -1,5 +1,6 @@
+import { Hero } from '~/common/components/hero';
 import type { Route } from './+types/community-page';
-import { Form, Link, useSearchParams } from 'react-router';
+import { Await, Form, Link, useSearchParams } from 'react-router';
 import { Button } from '~/common/components/ui/button';
 import {
   DropdownMenu,
@@ -10,28 +11,31 @@ import {
 import { ChevronDownIcon } from 'lucide-react';
 import { PERIOD_OPTIONS, SORT_OPTIONS } from '../constants';
 import { Input } from '~/common/components/ui/input';
-
-import { Hero } from '~/common/components/Hero';
-import { PostCard } from '~/features/products/components/post-card';
+import { PostCard } from '../components/post-card';
 import { getPosts, getTopics } from '../queries';
-import { posts } from '../schema';
+import { Suspense } from 'react';
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: 'Community | wemake' }];
 };
 
 export const loader = async () => {
+  // await new Promise((resolve) => setTimeout(resolve, 10000));
+  // const topics = await getTopics();
+  // const posts = await getPosts();
+  // const [topics, posts] = await Promise.all([getTopics(), getPosts()]);
   const topics = await getTopics();
-  const posts = await getPosts();
+  const posts = getPosts();
   return { topics, posts };
 };
 
 export default function CommunityPage({ loaderData }: Route.ComponentProps) {
+  const { topics, posts } = loaderData;
   const [searchParams, setSearchParams] = useSearchParams();
   const sorting = searchParams.get('sorting') || 'newest';
   const period = searchParams.get('period') || 'all';
   return (
-    <div>
+    <div className="space-y-20">
       <Hero
         title="Community"
         subtitle="Ask questions, share ideas, and connect with other developers"
@@ -100,28 +104,34 @@ export default function CommunityPage({ loaderData }: Route.ComponentProps) {
               <Link to={`/community/submit`}>Create Discussion</Link>
             </Button>
           </div>
-          <div className="space-y-5">
-            {loaderData.posts.map((post) => (
-              <PostCard
-                key={post.post_id}
-                id={post.post_id}
-                title={post.title}
-                author={post.author}
-                authorAvatarUrl={post.author_avatar!}
-                category={post.topic}
-                postedAt={post.created_at}
-                votesCount={post.upvotes}
-                expanded
-              />
-            ))}
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Await resolve={posts}>
+              {(data) => (
+                <div className="space-y-5">
+                  {data.map((post) => (
+                    <PostCard
+                      key={post.post_id}
+                      id={post.post_id}
+                      title={post.title}
+                      author={post.author}
+                      authorAvatarUrl={post.author_avatar}
+                      category={post.topic}
+                      postedAt={post.created_at}
+                      votesCount={post.upvotes}
+                      expanded
+                    />
+                  ))}
+                </div>
+              )}
+            </Await>
+          </Suspense>
         </div>
         <aside className="col-span-2 space-y-5">
           <span className="text-sm font-bold text-muted-foreground uppercase">
             Topics
           </span>
           <div className="flex flex-col gap-2 items-start">
-            {loaderData.topics.map((topic) => (
+            {topics.map((topic) => (
               <Button
                 asChild
                 variant={'link'}
