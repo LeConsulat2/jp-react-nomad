071e281947697541bfe7e728dbbb0ae84810f0d1
LeConsulat2 <wooine2@gmail.com>
2025-05-16 17:23:49 +1200

    Log In Done

--- app/features/auth/pages/login-page.tsx
+++ app/features/auth/pages/login-page.tsx
@@ -1,4 +1,4 @@
-import { Form, useNavigation } from 'react-router';
+import { Form, redirect, useNavigation } from 'react-router';
 import { Link } from 'react-router';
 
 import { Button } from '~/common/components/ui/button';
@@ -7,6 +7,7 @@ import type { Route } from './+types/login-page';
 import AuthButtons from '../components/auth-buttons';
 import { makeSSRClient } from '~/supa-client';
 import { LoaderCircle } from 'lucide-react';
+import { z } from 'zod';
 
 export const meta: Route.MetaFunction = () => {
   return [
@@ -15,13 +16,49 @@ export const meta: Route.MetaFunction = () => {
   ];
 };
 
+const formSchema = z.object({
+  email: z
+    .string({
+      required_error: 'Email is required',
+      invalid_type_error: 'Email is invalid',
+    })
+    .email(),
+  password: z
+    .string({
+      required_error: 'Password is required',
+      invalid_type_error: 'Password is invalid',
+    })
+    .min(8, {
+      message: 'Password must be at least 8 characters long',
+    }),
+});
+
 export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
-  const email = formData.get('email');
-  const password = formData.get('password');
-  return {
-    message: 'Error wrong email or password',
-  };
+  const { success, data, error } = formSchema.safeParse(
+    Object.fromEntries(formData),
+  );
+
+  if (!success) {
+    return {
+      loginError: null,
+      formErrors: error.flatten().fieldErrors,
+    };
+  }
+  const { email, password } = data;
+  const { client, headers } = makeSSRClient(request);
+  const { data: user, error: loginError } =
+    await client.auth.signInWithPassword({
+      email,
+      password,
+    });
+  if (loginError) {
+    return {
+      formErrors: null,
+      loginError: loginError.message,
+    };
+  }
+  return redirect('/', { headers });
 };
 
 export default function LoginPage({ actionData }: Route.ComponentProps) {
@@ -44,6 +81,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             type="email"
             placeholder="i.e wemake@example.com"
           />
+          {actionData && 'formErrors' in actionData && (
+            <p className="text-sm text-red-500">
+              {actionData.formErrors?.email?.join(', ')}
+            </p>
+          )}
           <InputPair
             id="password"
             label="Password"
@@ -53,6 +95,11 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
             type="password"
             placeholder="i.e wemake@example.com"
           />
+          {actionData && 'formErrors' in actionData && (
+            <p className="text-sm text-red-500">
+              {actionData?.formErrors?.password?.join(', ')}
+            </p>
+          )}
           <Button className="w-full" type="submit" disabled={isSubmitting}>
             {isSubmitting ? (
               <LoaderCircle className="animate-spin" />
@@ -60,8 +107,8 @@ export default function LoginPage({ actionData }: Route.ComponentProps) {
               'Log in'
             )}
           </Button>
-          {actionData?.message && (
-            <p className="text-sm text-red-500">{actionData.message}</p>
+          {actionData && 'loginError' in actionData && (
+            <p className="text-sm text-red-500">{actionData.loginError}</p>
           )}
         </Form>
         <AuthButtons />
