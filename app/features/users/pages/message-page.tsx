8462f7f499cca2257ceb130699faa0c1e2f8459b
LeConsulat2 <wooine2@gmail.com>
2025-05-23 15:50:37 +1200

    Send Message

--- app/features/users/pages/message-page.tsx
+++ app/features/users/pages/message-page.tsx
@@ -10,39 +10,67 @@ import {
   AvatarFallback,
   AvatarImage,
 } from '~/common/components/ui/avatar';
-import { Form } from 'react-router';
+import { Form, useOutletContext } from 'react-router';
 import { Textarea } from '~/common/components/ui/textarea';
 import { Button } from '~/common/components/ui/button';
 import { SendIcon } from 'lucide-react';
 import { MessageBubble } from '../components/messages-bubble';
+import { makeSSRClient } from '~/supa-client';
+import {
+  getLoggedInUserId,
+  getMessagesByMessagesRoomId,
+  getRoomsParticipant,
+} from '../queries';
 
 export const Meta: Route.MetaFunction = () => [
   { title: 'Message | We-Create' },
 ];
 
-export default function MessagePage() {
+export const loader = async ({ request, params }: Route.LoaderArgs) => {
+  const { client } = await makeSSRClient(request);
+  const userId = await getLoggedInUserId(client as any);
+  const messages = await getMessagesByMessagesRoomId(client as any, {
+    messageRoomId: params.messageRoomId,
+    userId,
+  });
+  const participants = await getRoomsParticipant(client as any, {
+    messageRoomId: params.messageRoomId,
+    userId,
+  });
+  return {
+    messages,
+    participants,
+  };
+};
+
+export default function MessagePage({ loaderData }: Route.ComponentProps) {
+  const { userId } = useOutletContext<{ userId: string }>();
   return (
     <div className="h-full flex flex-col justify-between">
       <Card>
         <CardHeader className="flex flex-row items-center gap-4">
           <Avatar className="size-12">
-            <AvatarImage src="https://github.com/serranoarevalo.png" />
-            <AvatarFallback>SA</AvatarFallback>
+            <AvatarImage src={loaderData.participants?.profile?.avatar ?? ''} />
+            <AvatarFallback>
+              {loaderData.participants?.profile?.name.charAt(0) ?? ''}
+            </AvatarFallback>
           </Avatar>
-          <div className="flex flex-col gap-1">
-            <CardTitle>John Doe</CardTitle>
-            <CardDescription>2 minutes ago</CardDescription>
+          <div className="flex flex-col gap-0">
+            <CardTitle>
+              {loaderData.participants?.profile?.name ?? ''}
+            </CardTitle>
+            <CardDescription>2 days ago</CardDescription>
           </div>
         </CardHeader>
       </Card>
-      <div className="py-4 overflow-y-scroll flex flex-col justify-start h-full">
-        {Array.from({ length: 20 }).map((_, index) => (
+      <div className="py-4 overflow-y-scroll space-y-4 flex flex-col justify-start h-full">
+        {loaderData.messages.map((message) => (
           <MessageBubble
-            key={index}
-            avatarUrl="https://github.com/serranoarevalo.png"
-            avatarFallback="SA"
-            content="This is JP and I am an admin but know how to code"
-            isCurrentUser={index % 2 === 0}
+            key={message.message_id}
+            avatarUrl={message.sender?.avatar ?? ''}
+            avatarFallback={message.sender?.name?.charAt(0) ?? ''}
+            content={message.content}
+            isCurrentUser={message.sender?.profile_id === userId}
           />
         ))}
       </div>
--- app/features/users/queries.ts
+++ app/features/users/queries.ts
@@ -1,19 +1,27 @@
-import pkg from '@supabase/supabase-js';
 import type { SupabaseClient } from '@supabase/supabase-js';
-import { productListSelect } from '../products/queries';
 import type { Database } from '~/supa-client';
+import { productListSelect } from '../products/queries';
 import { redirect } from 'react-router';
 
 export const getUserProfile = async (
-  client: pkg.SupabaseClient<Database>,
-  username: string,
+  client: SupabaseClient<Database>,
+  { username }: { username: string },
 ) => {
   const { data, error } = await client
     .from('profiles')
-    .select(`profile_id, name, username, avatar, role, headline, bio`)
+    .select(
+      `
+        profile_id,
+        name,
+        username,
+        avatar,
+        role,
+        headline,
+        bio
+        `,
+    )
     .eq('username', username)
     .single();
-
   if (error) {
     throw error;
   }
@@ -21,7 +29,7 @@ export const getUserProfile = async (
 };
 
 export const getUserById = async (
-  client: pkg.SupabaseClient<Database>,
+  client: SupabaseClient<Database>,
   { id }: { id: string },
 ) => {
   const { data, error } = await client
@@ -33,7 +41,7 @@ export const getUserById = async (
         username,
         avatar,
         headline,
-        bio, 
+        bio,
         role
         `,
     )
@@ -46,18 +54,18 @@ export const getUserById = async (
 };
 
 export const getUserProducts = async (
-  client: pkg.SupabaseClient<Database>,
-  username: string,
+  client: SupabaseClient<Database>,
+  { username }: { username: string },
 ) => {
   const { data, error } = await client
     .from('products')
     .select(
       `
-          ${productListSelect},
-          profiles!products_to_profiles!inner (
-              profile_id
-          )
-      `,
+        ${productListSelect},
+        profiles!products_to_profiles!inner (
+            profile_id
+        )
+    `,
     )
     .eq('profiles.username', username);
   if (error) {
@@ -67,8 +75,8 @@ export const getUserProducts = async (
 };
 
 export const getUserPosts = async (
-  client: pkg.SupabaseClient<Database>,
-  username: string,
+  client: SupabaseClient<Database>,
+  { username }: { username: string },
 ) => {
   const { data, error } = await client
     .from('community_post_list_view')
@@ -103,32 +111,34 @@ export const getProductsByUserId = async (
 };
 
 export const getNotifications = async (
-  client: pkg.SupabaseClient<Database>,
+  client: SupabaseClient<Database>,
   { userId }: { userId: string },
 ) => {
   const { data, error } = await client
     .from('notifications')
     .select(
-      `notification_id,
-    type,
-    source:profiles!source_id(
-    profile_id,
-    name,
-    avatar
-    ),
-    product:products!product_id(
-      product_id,
-      name
-    ),
-    post:posts!post_id(
-      post_id,
-      title
-    ),
-    seen,
-    created_at
-    `,
+      `
+      notification_id,
+      type,
+      source:profiles!source_id(
+        profile_id,
+        name,
+        avatar
+      ),
+      product:products!product_id(
+        product_id,
+        name
+      ),
+      post:posts!post_id(
+        post_id,
+        title
+      ),
+      seen,
+      created_at
+      `,
     )
-    .eq('target_id', userId);
+    .eq('target_id', userId)
+    .order('created_at', { ascending: false });
   if (error) {
     throw error;
   }
@@ -136,7 +146,7 @@ export const getNotifications = async (
 };
 
 export const countNotifications = async (
-  client: pkg.SupabaseClient<Database>,
+  client: SupabaseClient<Database>,
   { userId }: { userId: string },
 ) => {
   const { count, error } = await client
@@ -151,7 +161,7 @@ export const countNotifications = async (
 };
 
 export const getMessages = async (
-  client: pkg.SupabaseClient<Database>,
+  client: SupabaseClient<Database>,
   { userId }: { userId: string },
 ) => {
   const { data, error } = await client
@@ -159,6 +169,74 @@ export const getMessages = async (
     .select('*')
     .eq('profile_id', userId)
     .neq('other_profile_id', userId);
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const getMessagesByMessagesRoomId = async (
+  client: SupabaseClient<Database>,
+  { messageRoomId, userId }: { messageRoomId: string; userId: string },
+) => {
+  const { count, error: countError } = await client
+    .from('message_room_members')
+    .select('*', { count: 'exact', head: true })
+    .eq('message_room_id', messageRoomId)
+    .eq('profile_id', userId);
+  if (countError) {
+    throw countError;
+  }
+  if (count === 0) {
+    throw new Error('Message room not found');
+  }
+  const { data, error } = await client
+    .from('messages')
+    .select(
+      `*,
+      sender:profiles!sender_id!inner(
+        name,
+        profile_id,
+        avatar
+      )
+      `,
+    )
+    .eq('message_room_id', messageRoomId)
+    .order('created_at', { ascending: true });
+  if (error) {
+    throw error;
+  }
+  return data;
+};
+
+export const getRoomsParticipant = async (
+  client: SupabaseClient<Database>,
+  { messageRoomId, userId }: { messageRoomId: string; userId: string },
+) => {
+  const { count, error: countError } = await client
+    .from('message_room_members')
+    .select('*', { count: 'exact', head: true })
+    .eq('message_room_id', messageRoomId)
+    .eq('profile_id', userId);
+  if (countError) {
+    throw countError;
+  }
+  if (count === 0) {
+    throw new Error('Message room not found');
+  }
+  const { data, error } = await client
+    .from('message_room_members')
+    .select(
+      `
+      profile:profiles!profile_id!inner(
+        name,
+        avatar
+      )
+      `,
+    )
+    .eq('message_room_id', messageRoomId)
+    .neq('profile_id', userId)
+    .single();
   if (error) {
     throw error;
   }
